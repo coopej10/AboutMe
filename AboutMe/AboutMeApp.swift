3f271d8dc084d906a36de30e194cb851509e929b
Jessica Cooper <cooperjessica96@gmail.com>
2025-04-13 18:03:18 -0400

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Jessica Cooper on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
