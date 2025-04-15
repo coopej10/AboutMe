423ee82f61ef194043d2acf4064955c2e07c5e31
Jessica Cooper <cooperjessica96@gmail.com>
2025-04-15 17:54:58 -0400

    Added a button with a fun fact message

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -8,21 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        ScrollView {
-            ZStack {
-                Color(red:248/255,green:200/255,blue:220/255 )
+    @State private var buttonText = "Tap for a fun fact"
+var body: some View {
+    ScrollView {
+        ZStack {
+            Color(red:248/255,green:200/255,blue:220/255 )
                 .ignoresSafeArea()
-                VStack {
-                    Text("All About Me!")
-                        .font(.title)
-                        .fontWeight(.medium)
-                        .foregroundColor(Color.red)
-                    Image("cherryBlossoms")
-                        .resizable(resizingMode: .stretch)
-                        .aspectRatio(contentMode: .fit)
-                        .cornerRadius(15)
-                        .padding(.all)
+        VStack {
+            Text("All About Me!")
+                    .font(.title)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.red)
+            Image("cherryBlossoms")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                    .padding(.all)
+            Text("Welcome to my About Me app. I'm happy you're here!")
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    .padding([.top, .leading, .bottom])
+            Button(buttonText) {
+                buttonText = "I have visited more than 10 different countries!"
+                                }
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
                 }
             }
         }
